af15f20764dcd094387814f1290402a38c2df857
Razo <raz.miqayelyan@gmail.com>
2021-03-09 21:30:53 +0400

    tnayin 9.mart.2021

---
+++ homework7.py
@@ -0,0 +1,98 @@
+'''1.Calculator'''
+
+# def calculator():
+# 	while True:
+# 		number = input('Number:')
+# 		if number.isdigit():
+# 			number = int(number)
+# 			return number
+# 		else:
+# 			return('Only numbers')
+
+# x = calculator()
+# y = calculator()
+# gorcoxutyuny = input('*\n/\n+\n-\n ')
+# if gorcoxutyuny == '*':
+# 	print(x * y)
+# elif gorcoxutyuny == '+':
+# 	print(x + y)
+# elif gorcoxutyuny == '-':
+# 	print(x - y)
+# elif gorcoxutyuny == '/':
+# 	print(x / y)
+
+
+'''2.Write a python program to find max of two
+numbers.'''
+# def maxim(x, y):
+# 	if x > y:
+# 		print(x)
+# 	elif x < y:
+# 		print(y)
+# 	else:
+# 		print('Tvery havasar en')
+# maxim(22,33)		
+
+'''3.Write a python program to sum all numbers.'''
+# def all(*args):
+# 	total = 0
+# 	for i in args:
+# 		total += i
+# 	print(total)
+# all(22, 33, 44, 55)
+
+'''4.Write a python program to multiply all numbers.'''
+# def all(*args):
+# 	total = 1
+# 	for i in args:
+# 		total *= i
+# 	print(total)
+# all(22, 33, 44, 55)
+
+'''5.Write a python program to sum all letter and
+number in your string.'''
+# def all(string):
+# 	total = 0
+# 	res = []
+# 	for i in string:
+# 		if i.isdigit():
+# 			i = int(i)
+# 			total += i
+# 		elif i.isalpha():
+# 			res.append(i)
+# 	print(total)
+# 	for i in res:
+# 		print(i, end='')
+# all('wjfkbwnjf324215j3251jh2br')
+
+'''6.You are given a program that takes all 3
+passengers ages as inputs and inserts them in
+a list. Complete the program so that If it
+finds a value less than 16, it breaks the
+loop and outputs "Too young! ".
+If the age requirement is satisfied, the
+program outputs "Get ready!".'''
+
+# def pasajir(age):
+# 		if age < 16:
+# 			print('So young')
+		
+# 		else:
+# 			print('Are you ready?')
+			
+	
+# pasajir(20)
+# pasajir(19)
+# pasajir(12) 
+		
+
+	
+
+
+
+
+
+
+
+
+
