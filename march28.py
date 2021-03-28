c4960fff5cc66cb21facd5e2fc42489b6f1a36a4
Razo <raz.miqayelyan@gmail.com>
2021-03-28 13:14:13 +0400

    28.march.2021

---
+++ march28.py
@@ -0,0 +1,35 @@
+'''1. Create a python function factorial and import
+ this file in another file and print factorial.'''
+
+from forimport import f
+print(f(7))
+
+'''2.Write a Python function to calculate surface volume and area of
+a cylinder(Գլան).
+V=πr^2h and A=2πrh+2πr^2 :'''
+
+from forimport import glan
+print(glan(4,5))
+
+'''3. Write a Python function to calculate surface volume and area of a sphere(Գունդ). V = 4/3*π*r3 and A = 4*π*r2'''
+
+from forimport import gund 
+print(gund(11))
+
+'''4. Write a Python function to convert degree to radian 
+one radian = pi/180:
+90 radian = 1.57...'''
+
+from forimport import radian
+z = radian(90)
+print(round(z, 3))
+
+
+'''5. Write a Python function to print
+all primes smaller than or equal to a
+specified number.
+Call function:numbers(9)
+Output: (1, 3, 5, 7)'''
+
+from forimport import parz
+print(parz(9))
