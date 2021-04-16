20c0ad5c69df378720f8368982744a885152714f
Razo <raz.miqayelyan@gmail.com>
2021-04-16 23:19:59 +0400

    hello

---
+++ zadacha.py
@@ -0,0 +1,18 @@
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+# for i in a:
+# 	if i < 5:
+# 		print(i)
+
+# a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+# for i in a:
+# 	if i in b:
+# 		print(i)
+
+# my_dict = {'a':500, 'b':5874, 'c': 560,'d':400, 'e':5874, 'f': 20}
+# result = sorted(my_dict, key=my_dict.get, reverse=True)[:3]
+# print(result)
+
+print(int('ABC', 22))
